dc2ef2799289852f1ede7fc00059efd47233c662
Mohamed Abdelhakem <mohamed.abdelhakem3200@gmail.com>
2024-07-28 10:41:55 +0300

    from dell inspiron

---
+++ app/(component)/portfolio/(layout)/services/page.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Services() {
+    return (
+        <>
+            <section className="mt-16"></section>
+            <div className="container px-3">
+                <h3 className="titel">Services</h3>
+
+                <p className="mx-auto w-8/12 py-2 text-center font-semibold leading-7">
+                    Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex
+                    aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos
+                    quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia
+                    fugiat sit in iste officiis commodi quidem hic quas.
+                </p>
+            </div>
+        </>
+    );
+}
